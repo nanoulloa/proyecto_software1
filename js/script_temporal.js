307919779c73b236d9c7d086f0213b5047bfe228
josegit321 <josedanielcostarica@gmail.com>
2025-03-31 09:14:43 -0600

    index

--- js/script_temporal.js
+++ js/script_temporal.js
@@ -8,16 +8,14 @@ const $formulario = document.getElementById('formulario_usuario');
 
 const expresiones ={
     usuario:/^[a-zA-Z0-9\_\-]{4,16}$/, //SOLO ADMITE LETRAS MAYUSCULAS Y MINUSCULAS, GUION Y GUION BAJO Y NUMEROS, RANGO 4 CARACTERES MINIMO Y 16 MAXIMO
-    password: /^.{4,12}$/, //ACEPTA TODO PERO UN MINIMO DE 4 Y MAXIMO 12
-    correo:/^[a-zA-Z0-9\_]+@[a-zA-Z]+\.[a-zA-Z]+$/ , //VALIDAR LA ESTRUCTURA dato1@dato2.dato3  dato1=ACEPTA LETRAS MAYUSCULAS Y MINUSCULAS, NUMEROS Y GUION BAJO, dato2=ACEPTA SOLO LETRAS dato3=ACEPTA SOLO LETRAS
+    direcion_exacta: /^.{1,500}$/, //ACEPTA TODO PERO UN MINIMO DE 4 Y MAXIMO 12
     telefono: /^\d{8,11}$/
 }
 
 //Objeto CAMPOS
 const campos={
     usuario: false,
-    nombre: false,
-    password: false,
+    direcion_exacta: false,
     correo:false,
     telefono:false
 }
@@ -30,15 +28,8 @@ const validarFormulario = (e) =>{
             //           /^[a-zA-Z0-9\_\-]{4,16}$/
             validarCampo(expresiones.usuario, e.target, "usuario" );
         break;
-        case "password":
-            validarCampo(expresiones.password, e.target, "password");
-            validarPassword2();
-        break;
-        case "password2":
-            validarPassword2();
-        break;
-        case "correo":
-            validarCampo(expresiones.correo, e.target, "correo");
+        case "direcion_exacta":
+            validarCampo(expresiones.direcion_exacta, e.target, "direcion_exacta");
         break;
         case "telefono":
             validarCampo(expresiones.telefono, e.target, "telefono");
@@ -72,27 +63,6 @@ const validarCampo = (expresion,input,campo) =>{
 }
 
 
-const validarPassword2 = () =>{
-    let inputPassword1 = document.getElementById("password");
-    let inputPassword2 = document.getElementById("password2");
-
-    if(inputPassword1.value !== inputPassword2.value){
-        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-incorrecto");
-        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-correcto");
-        document.querySelector(`#grupo__password2 i`).classList.add("bxs-x-circle");
-        document.querySelector(`#grupo__password2 i`).classList.remove("bxs-check-circle");
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add("formulario__input-error-activo");
-        campos[password]=false;
-    }else{
-        document.getElementById(`grupo__password2`).classList.remove("formulario__grupo-incorrecto");
-        document.getElementById(`grupo__password2`).classList.add("formulario__grupo-correcto");
-        document.querySelector(`#grupo__password2 i`).classList.remove("bxs-x-circle");
-        document.querySelector(`#grupo__password2 i`).classList.add("bxs-check-circle");
-        document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove("formulario__input-error-activo");
-        campos[password]=true;
-    }
-}
-
 
 
 //PASO 2 ESCUCHAR EL EVENTO
@@ -111,7 +81,7 @@ $formulario.addEventListener("submit",(e)=>{
     e.preventDefault();//Evita que se recargue la p+agina
 
     const $terminos = document.getElementById("terminos");
-    if(campos.correo && campos.nombre && campos.password && campos.telefono && campos.usuario && $terminos.checked){
+    if(campos.direcion_exacta && campos.telefono && campos.usuario.checked){
         document.getElementById("formulario__mensaje").classList.remove("formulario__mensaje-activo");
         document.getElementById("formulario__mensaje-exito").classList.add("formulario__mensaje-exito-activo");
 
