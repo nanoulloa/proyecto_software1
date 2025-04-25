0a7bd9792a0dd785fbf6f1637300d03c5c6ade7d
nanoulloa <marianoburpy@gmail.com>
2025-04-25 02:02:27 -0600

    feat: update perfil

--- backend/routes/perfiUsuario.routes.js
+++ backend/routes/perfiUsuario.routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Registro = require('../models/registro.model');
+const registro_model = require('../models/registro.model');
 
 router.get('/perfil-usuario', (req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/public/pages/perfil_usuario/perfiUsuario.html'));
+});
+
+router.get('/mostrar-perfil-usuario', async (req, res) => {
+  try {
+    const perfil = await registro_model.find();
+    res.json(perfil); // ¡No pongas { noticias }, simplemente devuelve el arreglo!
+  } catch (error) {
+    res.status(500).json({ mensaje: 'Error al obtener perfil', error });
+  }
 });
 
 module.exports = router;
