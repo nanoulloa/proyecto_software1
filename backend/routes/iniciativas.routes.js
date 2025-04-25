aa24b2509bbb968f169c62a5ebd34091bb3bfadb
josegit321 <josedanielcostarica@gmail.com>
2025-04-24 22:52:36 -0600

    feat:update

--- backend/routes/iniciativas.routes.js
+++ backend/routes/iniciativas.routes.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
+function soloAdmin(req, res, next) {
+    if (req.user && req.user.rol === 'admin') {
+        next();
+    } else {
+        res.status(403).send("Acceso denegado");
+    }
+}
+
 router.get('/iniciativas', (req,res) => {
     res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativas-aprobadas.html'));
 });
 
 router.get('/crear-iniciativas',soloAdmin, (req,res) => {/////⚠️aun no esta linkeado en ningun lugar⚠️/////
-    res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativa.html'){
+    res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativa.html'),{
         usuario: req.user.nombreUsuario,
         rol: req.user.rol
     });
