9cce2f60043849068478d1d81337ac18dcce7fda
josegit321 <josedanielcostarica@gmail.com>
2025-04-24 22:49:10 -0600

    feat:update

--- backend/routes/iniciativas.routes.js
+++ backend/routes/iniciativas.routes.js
@@ -6,8 +6,11 @@ router.get('/iniciativas', (req,res) => {
     res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativas-aprobadas.html'));
 });
 
-router.get('/crear-iniciativas', (req,res) => {/////⚠️aun no esta linkeado en ningun lugar⚠️/////
-    res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativa.html'));
+router.get('/crear-iniciativas',soloAdmin, (req,res) => {/////⚠️aun no esta linkeado en ningun lugar⚠️/////
+    res.render(path.join(__dirname,'../../frontend/public/pages/iniciativas/iniciativa.html'){
+        usuario: req.user.nombreUsuario,
+        rol: req.user.rol
+    });
 });
 
 module.exports = router;
